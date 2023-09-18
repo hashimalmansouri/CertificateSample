de1779eb28529319bc76b858d48085573eb42671
Hashim Al-Mansouri <55322650+hashimalmansouri@users.noreply.github.com>
2023-09-18 18:33:06 +0300

    Add project files.

---
+++ CertificateSample/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using CertificateSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Reporting.NETCore;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace CertificateSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetFile(CertificateViewModel model)
+        {
+            var result = await GetReport(model);
+            return Json(new { success = true, data = result });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ExportReportToPdf(CertificateViewModel model)
+        {
+            var result = await GetReport(model);
+            var fileName = $"شهادة_{model.Name}.pdf";
+            return File(result, System.Net.Mime.MediaTypeNames.Application.Pdf, fileName);
+        }
+
+        public async Task<byte[]> GetReport(CertificateViewModel model)
+        {
+            using var report = new LocalReport();
+            using var rs = Assembly.GetExecutingAssembly().GetManifestResourceStream("CertificateSample.wwwroot.samples.rptCertificate.rdlc");
+            report.LoadReportDefinition(rs);
+            report.DataSources.Add(new ReportDataSource("dsCertificate",
+                new List<CertificateViewModel>() { model }));
+            var bytes = report.Render("PDF");
+            return bytes;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CertificateSample/Models/CertificateViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CertificateSample.Models
+{
+    public class CertificateViewModel
+    {
+        public string? Name { get; set; }
+        public string? Age { get; set; }
+        public string? Diagnose { get; set; }
+        public string? FileNo { get; set; }
+        public string? Address { get; set; }
+    }
+}
